76ea7a82efe73f432e1a22fccc3417f5383bd7cc
Gabriel Teixeira <gabrielteixeira1133@gmail.com>
2025-06-16 22:13:13 -0300

    fix:vercel3

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen bg-background text-foreground`}>{children}</body>
     </html>
   )
 }
